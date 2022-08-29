cad496586b0fa4a62b3728511806f5c3f17a7f5b
Haydn Boul <handyhaydn22@gmail.com>
2022-08-29 13:56:34 +1200

    Added Ultrasonic Sensor (Motor code is disabled)

--- PCBProjectCode/PCBProjectCode.ino
+++ PCBProjectCode/PCBProjectCode.ino
@@ -16,8 +16,26 @@ void setup() {
   Serial.begin(9600);
   Serial.println("<Arduino is ready>");
   pinMode(functionLED, OUTPUT);
+  pinMode(motorLeftA,OUTPUT); // Motor Setup
+  pinMode(motorLeftB,OUTPUT); // Motor Setup
+
+  pinMode(motorRightA,OUTPUT); // Motor Setup
+  pinMode(motorRightB,OUTPUT); // Motor Setup
+  
+  pinMode(sensorTrig, OUTPUT);
+  pinMode(sensorEcho, INPUT);
   
   Robot = WAITING;
+
+  rpm=0;
+  pulses=0;
+  time_old=0;
+
+  pinMode(encoderLeft,INPUT); // Setting encoder pin as Input
+  pinMode(encoderRight,INPUT); // Setting encoder pin as Input
+  
+  attachInterrupt(digitalPinToInterrupt(encoderLeft), count, FALLING ); // Triggering count function everytime the encoder pin1 turns from 1 to 0
+
     
 }
 // =================================
@@ -25,26 +43,79 @@ void setup() {
 void loop() {
   switch (Robot){
     case WAITING:
-    Serial.println("State: Waiting");
+//    Serial.println("State: Waiting");
       runWaitingLED();
       recvOneChar();
       showNewData();
       break;
 
     case EXECUTING:
-      Serial.println("State: Executing");
-      
+//      Serial.println("State: Executing");
+//      runMotors(20, 50);
+      checkDistance();
       break;
   }
 
-
-  delay(1000);
 }
 
+void count() // Counting the number of pulses for calculation of rpm
+{
+  pulses++;
+  movement++;  
+}
+
+void runMotors(int leftDistance, int rightDistance){
+    if (movement <= abs(leftDistance)) {
+      if (leftDistance < 0){
+        analogWrite(motorLeftA, 0); // Motor On, swap for other direction
+        analogWrite(motorLeftB, motorSpeed); // Motor On, swap for other direction
+      }
+      if (leftDistance >= 0){
+        analogWrite(motorLeftA, motorSpeed); // Motor On, swap for other direction
+        analogWrite(motorLeftB, 0); // Motor On, swap for other direction
+      }
+      
+    
+    if(millis()-time_old >=100){ // Updating every 0.1 seconds
+      detachInterrupt(digitalPinToInterrupt(encoderLeft));
+//      rpm = (60 * 100 / pulses_per_turn )/ (millis() - time_old)* pulses;
+      time_old= millis();
+//      pulses=0;
+//      Serial.print("RPM= ");
+//      Serial.println(rpm);
+      attachInterrupt(digitalPinToInterrupt(encoderLeft), count, FALLING ); // Triggering count function everytime the encoder pin1 turns from 1 to 0
+    }
+
+  }
+  else {
+    analogWrite(motorLeftA, 0); // Motor On, swap for other direction
+    analogWrite(motorLeftB, 0); // Motor On, swap for other direction
+    delay(1000);
+    movement = 0;
+  }
+}
+
+void checkDistance(){
+  // Clears the trigPin condition
+  digitalWrite(sensorTrig, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(sensorTrig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(sensorTrig, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(sensorEcho, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+}
 
 void runWaitingLED(){
 // Runs the status LED
-  if (waitingLEDCounter != 1){
+  if (waitingLEDCounter != 1000){
     digitalWrite(functionLED, HIGH);
     waitingLEDCounter++;
   } else{
--- PCBProjectCode/robot.h
+++ PCBProjectCode/robot.h
@@ -9,7 +9,17 @@ typedef enum {
 // ======= init vars ===============
 
 int waitingLEDCounter = 0;
-int movementLEDCounter = 0;
+
+int motorSpeed = 100;
+
+int movement = 0;
+unsigned int rpm;
+volatile byte pulses;
+unsigned long time_old;
+unsigned int pulses_per_turn= 20; // Depends on the number of spokes on the encoder wheel
+
+long duration; // variable for the duration of sound wave travel
+int distance; // variable for the distance measurement
 
 // =================================
 
@@ -18,8 +28,8 @@ const byte functionLED = A0;
 const byte leftMovementLED = 4;
 const byte rightMovementLED = 3;
 
-const byte encoderA = 2;
-const byte encoderB = A1;
+const byte encoderLeft = 2;
+const byte encoderRight = A1;
 
 const byte sensorTrig = A2;
 const byte sensorEcho = A3;
