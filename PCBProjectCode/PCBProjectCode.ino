99949219a22e5c02442c175aacfb097c3dc65808
Haydn Boul <handyhaydn22@gmail.com>
2022-09-07 15:56:44 +1200

    Added Motor and encoder implementation for left and right motor, as well as some formatting and cleaning

--- PCBProjectCode/PCBProjectCode.ino
+++ PCBProjectCode/PCBProjectCode.ino
@@ -19,56 +19,192 @@ newDirection robotDirection;
 void setup() {
   Serial.begin(9600);
   Serial.println("<Arduino is ready>");
+
   pinMode(functionLED, OUTPUT);
   pinMode(leftMovementLED, OUTPUT);
   pinMode(rightMovementLED, OUTPUT);
-  pinMode(motorLeftA,OUTPUT); // Motor Setup
-  pinMode(motorLeftB,OUTPUT); // Motor Setup
 
-  pinMode(motorRightA,OUTPUT); // Motor Setup
-  pinMode(motorRightB,OUTPUT); // Motor Setup
-  
+  pinMode(motorLeftA, OUTPUT);  // Motor Setup
+  pinMode(motorLeftB, OUTPUT);  // Motor Setup
+
+  pinMode(motorRightA, OUTPUT);  // Motor Setup
+  pinMode(motorRightB, OUTPUT);  // Motor Setup
+
   pinMode(sensorTrig, OUTPUT);
   pinMode(sensorEcho, INPUT);
-  
-  Robot = WAITING;
-  robotDirection = FORWARDS;
 
-  rpm=0;
-  pulses=0;
-  time_old=0;
+  pinMode(encoderLeft, INPUT);   // Setting encoder pin as Input
+  pinMode(encoderRight, INPUT);  // Setting encoder pin as Input
 
-  pinMode(encoderLeft,INPUT); // Setting encoder pin as Input
-  pinMode(encoderRight,INPUT); // Setting encoder pin as Input
-  
-  attachInterrupt(digitalPinToInterrupt(encoderLeft), count, FALLING ); // Triggering count function everytime the encoder pin1 turns from 1 to 0
-
-    
+  Robot = EXECUTING;
+  robotDirection = STATIONARY;
 }
 // =================================
 
 void loop() {
 
-  currentMillis = millis(); // For LED flashing
+  currentMillis = millis();  // For LED flashing
 
-  switch (Robot){
+  switch (Robot) {
     case WAITING:
-//    Serial.println("State: Waiting");
+      // Serial.println("State: Waiting");
       runWaitingLED();
-      runLeftMovementLED();
-      runRightMovementLED();
       // recvOneChar();
       // inputDirection();
       // inputDistance();
       break;
 
     case EXECUTING:
-//      Serial.println("State: Executing");
-     runMotors(20, 50);
-      // checkDistance();
+      //  Serial.println("State: Executing");
+      //  checkDistance();
+      runWaitingLED();
+      runLeftMovementLED();
+      runRightMovementLED();
+      runLeftMotor(0);
+      runRightMotor(20);
+
       break;
   }
-  // delay(1);
+}
+
+void runLeftMotor(int leftDistance) {
+  if (leftDistance == 0){
+    analogWrite(motorLeftA, 0);  // Motor On, swap for other direction
+    analogWrite(motorLeftB, 0);  // Motor On, swap for other direction
+    return;
+  }
+
+
+  if (encoderLeftCount <= abs(leftDistance)) {
+    if (leftDistance < 0) {
+      analogWrite(motorLeftA, 0);           // Motor On, swap for other direction
+      analogWrite(motorLeftB, motorSpeed);  // Motor On, swap for other direction
+    }
+    if (leftDistance > 0) {
+      analogWrite(motorLeftB, 0);           // Motor On, swap for other direction
+      analogWrite(motorLeftA, motorSpeed);  // Motor On, swap for other direction
+    }
+    encoderLeftState = digitalRead(encoderLeft);
+    if ((encoderLeftState == HIGH) && (encoderLeftStateOld == LOW)) {
+      encoderLeftCount++;
+    }
+    encoderLeftStateOld = encoderLeftState;
+
+  } else {
+    analogWrite(motorLeftA, 0);  // Motor On, swap for other direction
+    analogWrite(motorLeftB, 0);  // Motor On, swap for other direction
+    delay(500);
+    encoderLeftCount = 1;
+  }
+}
+
+
+void runRightMotor(int rightDistance) {
+    if (rightDistance == 0){
+    analogWrite(motorRightA, 0);  // Motor On, swap for other direction
+    analogWrite(motorRightB, 0);  // Motor On, swap for other direction
+    return;
+  }
+  if (encoderRightCount <= abs(rightDistance)) {
+    if (rightDistance < 0) {
+      analogWrite(motorRightA, 0);           // Motor On, swap for other direction
+      analogWrite(motorRightB, motorSpeed);  // Motor On, swap for other direction
+    }
+    if (rightDistance > 0) {
+      analogWrite(motorRightB, 0);           // Motor On, swap for other direction
+      analogWrite(motorRightA, motorSpeed);  // Motor On, swap for other direction
+    }
+    encoderRightState = digitalRead(encoderRight);
+    if ((encoderRightState == HIGH) && (encoderRightStateOld == LOW)) {
+      encoderRightCount++;
+    }
+    encoderRightStateOld = encoderRightState;
+
+  } else {
+    analogWrite(motorRightA, 0);  // Motor On, swap for other direction
+    analogWrite(motorRightB, 0);  // Motor On, swap for other direction
+    delay(500);
+    // Robot = WAITING;
+    encoderRightCount = 1;
+  }
+}
+
+void checkDistance() {
+  // Clears the trigPin condition
+  digitalWrite(sensorTrig, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
+  digitalWrite(sensorTrig, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(sensorTrig, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration = pulseIn(sensorEcho, HIGH);
+  // Calculating the distance
+  distance = duration * 0.034 / 2;  // Speed of sound wave divided by 2 (go and back)
+  // Displays the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.print(distance);
+  Serial.println(" cm");
+}
+
+void recvOneChar() {
+  //if (Serial.available() > 0) {
+  if ((counter % 3) == 0 and inputMovement == false) {
+    Serial.println("Input a movement:\nStraight (S)\nTurn (T)\n");
+    receivedChar = Serial.read();
+    if (receivedChar = 'S') {
+      robotMovement = STRAIGHT;
+      counter += 1;
+    } else if (receivedChar = 'T') {
+      robotMovement = TURNING;
+      counter += 1;
+    }
+    newData = true;
+  }
+  //}
+}
+
+void inputDirection() {
+  if (newData == true) {
+    if ((counter % 3) == 1) {
+      counter += 1;
+      if (robotMovement == TURNING) {
+        Serial.println("Input direction to turn robot:\nAnticlockwise (A)\nClockwise(C)");
+        directionChar = Serial.read();
+        if (directionChar == 'A') {
+          robotDirection = ANTICLOCKWISE;
+        } else {
+          robotDirection = CLOCKWISE;
+        }
+      } else if (robotMovement = STRAIGHT) {
+        Serial.println("Input direction to drive robot:\nForwards (F)\nReverse (R)");
+        directionChar = Serial.read();
+        if (directionChar == 'F') {
+          robotDirection = FORWARDS;
+        } else {
+          robotDirection = REVERSE;
+        }
+      }
+    }
+    inputDistance();
+  }
+}
+
+void inputDistance() {
+  if ((counter % 3) == 2) {
+    counter += 1;
+    if (robotMovement = TURNING) {
+      Serial.println("Input the amount you would like to turn the robot in degrees");
+      distanceChar = Serial.read();
+      robotDistance = (int)distanceChar;
+      Robot = EXECUTING;
+    } else {
+      Serial.println("Input the amount you would like to move the robot in centimetres");
+      distanceChar = Serial.read();
+      robotDistance = (int)distanceChar;
+      Robot = EXECUTING;
+    }
+  }
 }
 
 void flashFunctionLED() {
@@ -127,63 +263,8 @@ void solidLED(byte pin) {
   digitalWrite(pin, HIGH);
 }
 
-void count() // Counting the number of pulses for calculation of rpm
-{
-  pulses++;
-  movement++;  
-}
-
-void runMotors(int leftDistance, int rightDistance){
-    if (movement <= abs(leftDistance)) {
-      if (leftDistance < 0){
-        analogWrite(motorLeftA, 0); // Motor On, swap for other direction
-        analogWrite(motorLeftB, motorSpeed); // Motor On, swap for other direction
-      }
-      if (leftDistance >= 0){
-        analogWrite(motorLeftA, motorSpeed); // Motor On, swap for other direction
-        analogWrite(motorLeftB, 0); // Motor On, swap for other direction
-      }
-      
-    
-    if(millis()-time_old >=100){ // Updating every 0.1 seconds
-      detachInterrupt(digitalPinToInterrupt(encoderLeft));
-//      rpm = (60 * 100 / pulses_per_turn )/ (millis() - time_old)* pulses;
-      time_old= millis();
-//      pulses=0;
-//      Serial.print("RPM= ");
-//      Serial.println(rpm);
-      attachInterrupt(digitalPinToInterrupt(encoderLeft), count, FALLING ); // Triggering count function everytime the encoder pin1 turns from 1 to 0
-    }
-
-  }
-  else {
-    analogWrite(motorLeftA, 0); // Motor On, swap for other direction
-    analogWrite(motorLeftB, 0); // Motor On, swap for other direction
-    delay(1000);
-    movement = 0;
-  }
-}
-
-void checkDistance(){
-  // Clears the trigPin condition
-  digitalWrite(sensorTrig, LOW);
-  delayMicroseconds(2);
-  // Sets the trigPin HIGH (ACTIVE) for 10 microseconds
-  digitalWrite(sensorTrig, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(sensorTrig, LOW);
-  // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(sensorEcho, HIGH);
-  // Calculating the distance
-  distance = duration * 0.034 / 2; // Speed of sound wave divided by 2 (go and back)
-  // Displays the distance on the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-}
-
-void runWaitingLED(){
-  switch (Robot){
+void runWaitingLED() {
+  switch (Robot) {
     case WAITING:
       flashFunctionLED();
       break;
@@ -193,8 +274,8 @@ void runWaitingLED(){
   }
 }
 
-void runLeftMovementLED(){
-  switch (robotDirection){
+void runLeftMovementLED() {
+  switch (robotDirection) {
     case STATIONARY:
       digitalWrite(leftMovementLED, LOW);
       break;
@@ -207,8 +288,8 @@ void runLeftMovementLED(){
   }
 }
 
-void runRightMovementLED(){
-  switch (robotDirection){
+void runRightMovementLED() {
+  switch (robotDirection) {
     case STATIONARY:
       digitalWrite(rightMovementLED, LOW);
       break;
@@ -219,67 +300,4 @@ void runRightMovementLED(){
       flashRightMovementLED();
       break;
   }
-}
-
-
-void recvOneChar() {
-    //if (Serial.available() > 0) {
-      if ((counter % 3) == 0 and inputMovement == false) {
-      Serial.println("Input a movement:\nStraight (S)\nTurn (T)\n");
-        receivedChar = Serial.read();
-        if (receivedChar = 'S') {
-          robotMovement = STRAIGHT;
-          counter +=1;
-        } else if (receivedChar = 'T') {
-          robotMovement = TURNING;
-          counter +=1;
-        }
-        newData = true;
-        
-    }
-    //}
-}
-
-void inputDirection() {
-    if (newData == true) {
-      if ((counter % 3) == 1) {
-        counter += 1;
-      if (robotMovement == TURNING) {
-        Serial.println("Input direction to turn robot:\nAnticlockwise (A)\nClockwise(C)");
-        directionChar = Serial.read();
-        if (directionChar == 'A') {
-          robotDirection = ANTICLOCKWISE;
-        } else {
-          robotDirection = CLOCKWISE;
-        }
-      } else if (robotMovement = STRAIGHT) {
-        Serial.println("Input direction to drive robot:\nForwards (F)\nReverse (R)");
-        directionChar = Serial.read();
-        if (directionChar == 'F') {
-          robotDirection = FORWARDS; 
-        } else {
-          robotDirection = REVERSE;
-        }
-      }
-    }
-    inputDistance();
-    }
-}
-
-void inputDistance() {
-  if ((counter % 3) == 2) {
-      counter += 1;
-  if (robotMovement = TURNING) {
-    Serial.println("Input the amount you would like to turn the robot in degrees");
-    distanceChar = Serial.read();
-    robotDistance = (int) distanceChar;
-    Robot = EXECUTING;
-  } else {
-    Serial.println("Input the amount you would like to move the robot in centimetres");
-    distanceChar = Serial.read();
-    robotDistance = (int) distanceChar;
-    Robot = EXECUTING;
-  }
-  }
-  
 }
--- PCBProjectCode/robot.h
+++ PCBProjectCode/robot.h
@@ -3,7 +3,7 @@ typedef enum {
   WAITING,
   EXECUTING,
   UNTETHERED
-}robotState;
+} robotState;
 // =================================
 
 // ======= init vars ===============
@@ -21,60 +21,61 @@ typedef enum {
   ANTICLOCKWISE
 } newDirection;
 
-int waitingLEDCounter = 0;
-
 int motorSpeed = 100;
 
 const long LEDFlashInterval = 500;  // interval at which to blink (milliseconds)
 unsigned long currentMillis;
 
 // =========== init function LED ============
-int functionLEDState = LOW; 
+int functionLEDState = LOW;
 unsigned long functionLEDPreviousMillis = 0;
 
 // ==========================================
 
 // =========== init leftMotor LED ===========
-int leftMotorLEDState = LOW; 
+int leftMotorLEDState = LOW;
 unsigned long leftMotorPreviousMillis = 0;
 
 // ==========================================
 
 // =========== init rightMotor LED ===========
-int rightMotorLEDState = LOW; 
+int rightMotorLEDState = LOW;
 unsigned long rightMotorPreviousMillis = 0;
-
 // ==========================================
 
+// ========== encoderLeft init =============
+int encoderLeftCount = 1;
+int encoderLeftState;
+int encoderLeftStateOld = LOW;
+// ==========================================
+
+// ========== encoderRight init =============
+int encoderRightCount = 1;
+int encoderRightState;
+int encoderRightStateOld = LOW;
+// ==========================================
 
 int movement = 0;
-unsigned int rpm;
-volatile byte pulses;
-unsigned long time_old;
-unsigned int pulses_per_turn= 20; // Depends on the number of spokes on the encoder wheel
 
-long duration; // variable for the duration of sound wave travel
-int distance; // variable for the distance measurement
+long duration;  // variable for the duration of sound wave travel
+int distance;   // variable for the distance measurement
 
 // =================================
 
 // ======= init pins ===============
-const byte functionLED = A0;
-const byte leftMovementLED = 4;
-const byte rightMovementLED = 3;
+#define functionLED A0      // Status LED
+#define leftMovementLED 4   // Left LED
+#define rightMovementLED 3  // Right LED
 
-const byte encoderLeft = 2;
-const byte encoderRight = A1;
+#define encoderLeft 2    // Left Encoder Data pin
+#define encoderRight A1  // Right Encoder Data pin
 
-const byte sensorTrig = A2;
-const byte sensorEcho = A3;
+#define sensorTrig A2  // Ultrasonic Sensor Trigger pin
+#define sensorEcho A3  // Ultrasonic Sensor Data pin
 
-const byte motorLeftA = 10; //PWM
-const byte motorLeftB = 9; //PWM
+#define motorLeftA 10  //PWM
+#define motorLeftB 9   //PWM
 
-const byte motorRightA = 6; //PWM
-const byte motorRightB = 5; //PWM
-
-
-
-// =================================
+#define motorRightA 6  //PWM
+#define motorRightB 5  //PWM
+                       // =================================
